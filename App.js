19d8f60f905d256d458129a13f612f18a5ad4588
snadi043 <snadi043@uottawa.ca>
2024-11-25 13:30:08 -0500

    Initial commit for the changes while setting up the development environment for building react-native-game application.

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { StatusBar } from "react-native-web";
 
 export default function App() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Universal React with Expo</Text>
+      <Text>Hello World!!!</Text>
+      <StatusBar style="auto"/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+});
