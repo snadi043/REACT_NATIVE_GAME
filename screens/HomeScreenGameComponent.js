3248b4be21df3d6637486bd1092c0dcbcbd23e82
snadi043 <snadi043@uottawa.ca>
2024-11-25 18:06:32 -0500

    Initial changes in terms of stylings and displaying the data in the HomescreenGame component file.

--- screens/HomeScreenGameComponent.js
+++ screens/HomeScreenGameComponent.js
@@ -1,12 +1,26 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import CustomizedButton from "../components/CustomizedButton";
 
-function HomeScreenGameComponent(){
-    return <View>
-        <TextInput/>
-        <CustomizedButton>Reset</CustomizedButton>
-        <CustomizedButton>Confirm</CustomizedButton>
-
-    </View>
+function HomescreenGameComponent(){
+    return <View styles={styles.inputContainer}>
+            <TextInput/>
+                <CustomizedButton>Reset</CustomizedButton>
+                <CustomizedButton>Confirm</CustomizedButton>
+            </View>
 }
-export default HomeScreenGameComponent;
+export default HomescreenGameComponent;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginTop: 100,
+        padding: 16,
+        marginHorizontal: 24,
+        backgroundColor: '#72063C',
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOpacity: 0.25,
+        shadowOffset: {height: 2, width: 0},
+        shadowRadius: 6,
+        borderRadius: 8,
+    }
+})
